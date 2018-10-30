91777d9ea533fc2b5b8ccd2212f620564c5246f8
seyedsajjad <seyedalian10@gmail.com>
2018-10-30 19:57:44 +0330

    set get request in file Request

--- app/src/main/java/ir/malv/detfgit/network/request/Request.java
+++ app/src/main/java/ir/malv/detfgit/network/request/Request.java
@@ -1,5 +1,12 @@
 package ir.malv.detfgit.network.request;
 
+import ir.malv.detfgit.network.model.Rss;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface Request {
     // TODO: All your requests here.
+    @GET("14")
+    Call<Rss> getRss();
+
 }
