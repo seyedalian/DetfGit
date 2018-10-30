f0f80f54efc4b90d17c1a7255207a21d40313c82
seyedsajjad <seyedalian10@gmail.com>
2018-10-30 14:13:43 +0330

    delete model.java

--- app/src/main/java/ir/malv/detfgit/network/model/Model.java
+++
@@ -1,8 +0,0 @@
-package ir.malv.detfgit.network.model;
-
-public class Model {
-
-    // TODO : This class must be deleted.
-    // So keeping it will cause minus points.
-    // Thia class is made to make the model package be created at github
-}
