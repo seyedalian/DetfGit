9141100341e3af928a17a6bbcaedaff4b0d56eef
seyedsajjad <seyedalian10@gmail.com>
2018-10-31 16:28:29 +0330

    program is complete but model not match on models

--- app/src/main/java/ir/malv/detfgit/view/MainActivity.java
+++ app/src/main/java/ir/malv/detfgit/view/MainActivity.java
@@ -1,32 +1,82 @@
 package ir.malv.detfgit.view;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import ir.malv.detfgit.R;
+import ir.malv.detfgit.network.model.Enclosure;
+import ir.malv.detfgit.network.model.Item;
+import ir.malv.detfgit.network.model.Rss;
 import ir.malv.detfgit.network.provider.DataProvider;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     DataProvider dataProvider;
+    Rss rss;
+    Item[] items;
+    ListView listNews;
+    ItemAdapter adapter;
+    TextView title;
+    TextView link;
+    TextView lastBuildDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setDefaultToolbar();
-        initViews();
-
-
-        //simpleAlert("title", "message");
+        init();
+        //setOnClickListener for go to page of api
+        link.setOnClickListener(this);
 
 
     }
+    private void refreshDisplay() {
+        adapter = new ItemAdapter(MainActivity.this,R.layout.item_of_listview,items);
+        listNews.setAdapter(adapter);
 
-    /**
-     * Initialize XML views.
-     */
-    private void initViews() {
+    }
+
+
+
+
+    private void init() {
         dataProvider = DataProvider.getInstance();
+        listNews =findViewById(R.id.list_View);
+        title = findViewById(R.id.titleOfMainPage);
+        link = findViewById(R.id.linkOfMain);
+        lastBuildDate = findViewById(R.id.lastBuildDate);
+
+        rss = dataProvider.getRss();
+
+        if(rss != null){
+            items=rss.getChannel().getItem();
+            refreshDisplay();
+            title.setText(rss.getChannel().getTitle());
+            link.setText("go to page");
+            lastBuildDate.setText(rss.getChannel().getLastBuildDate());
+            String linkPage = rss.getChannel().getLink();
+            link.setTag(linkPage);
+        }
+
+
+
+
+
         // ...
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() ==R.id.linkOfMain){
+            String uri =view.getTag().toString();
+            Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+            startActivity(myIntent);
+        }
     }
 }
