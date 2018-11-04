2789e6451993335c067aa14d8dc2d260c026e932
seyedsajjad <seyedalian10@gmail.com>
2018-11-04 12:01:38 +0330

    add splash activity

---
+++ app/src/main/java/ir/malv/detfgit/view/SplashActivity.java
@@ -0,0 +1,20 @@
+package ir.malv.detfgit.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.malv.detfgit.R;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+
+
+    }
+}
