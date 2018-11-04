f35a89f4ddde32f7cb34abcd78148aaed2b59246
seyedsajjad <seyedalian10@gmail.com>
2018-11-04 12:15:58 +0330

    On branch master 	modified:   app/src/main/java/ir/malv/detfgit/view/SplashActivity.java : add typeface for font text 	modified:   app/src/main/res/layout/activity_splash.xml : edit ui 	modified:   app/src/main/res/values/strings.xml : add a text for splash activity

--- app/src/main/java/ir/malv/detfgit/view/SplashActivity.java
+++ app/src/main/java/ir/malv/detfgit/view/SplashActivity.java
@@ -1,18 +1,35 @@
 package ir.malv.detfgit.view;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import ir.malv.detfgit.R;
 
 public class SplashActivity extends BaseActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Typeface typeface = Typeface.createFromAsset(SplashActivity.this.getAssets(),"fonts/bnazanin.ttf");
+        textView =findViewById(R.id.textView);
+        textView.setTypeface(typeface);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
 
 
+
+            }
+        },2500l);
 
 
 
