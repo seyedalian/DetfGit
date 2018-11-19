63734b0c8178e1efee3af15551e868f6ad4c0d6e
seyedsajjad <seyedalian10@gmail.com>
2018-11-19 13:45:25 +0330

    On branch master  Changes to be committed: 	modified:   app/src/main/java/ir/malv/detfgit/Constant.java 	modified:   app/src/main/res/drawable/image.png 	modified:   app/src/main/res/layout/activity_splash.xml 	modified:   app/src/main/res/layout/item_of_listview.xml 	modified:   app/src/main/res/values/attrs.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/ir/malv/detfgit/Constant.java
+++ app/src/main/java/ir/malv/detfgit/Constant.java
@@ -2,7 +2,7 @@ package ir.malv.detfgit;
 
 public class Constant {
 
-    public static final String APP_NAME = "Detf Git";
+    public static final String APP_NAME = "Detf News";
     public static final String API_BASE_URL = "https://www.yjc.ir/fa/rss/";
 
 }
